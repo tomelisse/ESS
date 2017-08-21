319f536667676c711850b9a7ea9fa247fb871629
Maria Fabianska <tomelisse@gmail.com>
2017-08-21 13:47:28 +0200

    working version + testing prints

--- doves_and_hawks.py
+++ doves_and_hawks.py
@@ -23,43 +23,47 @@ class bird:
 
     def duel(self, opponent):
         ''' duel between 2 individuals '''
+        win       = self.__class__._win
+        wounds    = self.__class__._wounds
+        timewaste = self.__class__._timewaste
         if self.species == 'hawk' and opponent.species == 'hawk':
-            self.HP     += _win
-            opponent.HP += _wounds
+            self.HP     += win
+            opponent.HP += wounds
         if self.species == 'hawk' and opponent.species == 'dove':
-            self.HP     += _win
+            self.HP     += win
         if self.species == 'dove' and opponent.species == 'hawk':
-            opponent.HP += _win
+            opponent.HP += win
         if self.species == 'dove' and opponent.species == 'dove':
-            self.HP     += _win + _timewaste
-            opponent.HP += _timewaste
-
-    def time_flow(self):
-        ''' after a year has passed '''
-        self.age += 1
+            self.HP     += win + timewaste
+            opponent.HP += timewaste
+        print('duel: {} {}:{:d} vs {} {}:{:d}'.format(self.species, self.ID, self.HP, 
+                                                opponent.species, opponent.ID, opponent.HP))
 
     def litter(self):
         ''' litter size, dependant on HP'''
         if self.HP <= 0:
             return 0
-        if self.HP < 30:
+        if self.HP <= 50:
             return 1
-        if self.HP < 60:
+        if self.HP <= 100:
             return 2
         else:
             return 3
 
 class population:
+    _evolution_time  = 2
+    # number of duels wrt number of members
+    _duels_percentage = 0.5
     ''' class for a population of birds '''
     def __init__(self):
         ''' population composition '''
-        self.n_doves = 40
-        self.n_all   = 100
-        self.n_hawks = self.n_all - self.n_doves
+        n_doves = 2 
+        n_all   = 5
+        self.species = {'dove': 0, 'hawk' : 0, 'all' : 0}
         species = ['dove', 'hawk']
         self.members = dict()
-        for i in range(self.n_all):
-            index = i in range(self.n_doves, self.n_all)
+        for i in range(n_all):
+            index = i in range(n_doves, n_all)
             spec = species[index] 
             self.add_member(spec)
 
@@ -69,34 +73,85 @@ class population:
         while member.ID in self.members:
             member = bird(species)
         self.members[member.ID] = member
+        self.species[species] += 1
+        self.species['all'  ] += 1
 
-    def remove_member(self, id):
+    def remove_member(self, id, species):
         ''' remove the given member from the population '''
         del self.members[id]
+        self.species[species] -= 1
+        self.species['all']   -= 1
 
     def reproduction(self):
         ''' reproduction of individuals depending on their HP '''
-        for member in self.members:
+        print('==reproduction===')
+        for member in list(self.members.values()):
             litter_size = member.litter()
+            print('{} {} of HP:{:d} has {:d} children'.format(member.species, member.ID, member.HP, litter_size))
             for _ in range(litter_size):
                 species = member.species
                 self.add_member(species)
 
+    # TODO: add population size limit
     def extinction(self):
-        ''' old and injured members die out '''
-        for id, member in self.members.items():
-            if member.HP < 0 or member.age >= member.__class__.max_age:
-                remove_member(id)
+        print('=== extinction ===')
+        ''' old and injured members die out, others get older '''
+        for member in list(self.members.values()):
+            member.age += 1
+            if member.HP < 0 or member.age >= member.__class__._max_age:
+                print('extict: {}, HP: {:d}, age: {:d}'.format(member.ID, member.HP, member.age))
+                self.remove_member(member.ID, member.species)
+
+    def duels(self):
+        ''' set up duels between pairs of members '''
+        n_duels = int(self.__class__._duels_percentage*self.species['all'])
+        print('==={} duels ==='.format(n_duels))
+        for _ in range(n_duels):
+            player1_ID = random.choice(list(self.members.keys()))
+            player2_ID = random.choice(list(self.members.keys()))
+            while player2_ID == player1_ID:
+                player2_ID = random.choice(list(self.members.keys()))
+            self.members[player1_ID].duel(self.members[player2_ID])
+
+    def update_plots(self):
+        ''' update info about the number of each species '''
+        pass
+
+    def introduce_perturbation(self):
+        ''' check if a one-species population is stable '''
+        ''' by adding an admixture of the other species '''
+        pass
+
+    def print_members(self):
+        ''' print members '''
+        print('=== print members ===')
+        print('doves: ', self.species['dove'])
+        print('hawks: ', self.species['hawk'])
+        for member in list(self.members.values()):
+            print(member.ID, member.species, member.HP, member.age)
+
+    def evolution(self):
+        ''' simulates the evolution of the population '''
+        for epoch in range(self.__class__._evolution_time):
+            print('epoch: {}'.format(epoch))
+            self.print_members()
+            self.duels()
+            self.extinction()
+            self.reproduction()
+            self.update_plots()
+        self.print_members()
+
 
 def main():
     ''' main '''
     my_population = population()
-    doves = 0
-    for member in my_population.members.values():
-        print(member.ID, member.species)
-        if member.species == 'dove':
-            doves += 1
-    print(doves)
+    my_population.evolution()
+    # doves = 0
+    # for member in my_population.members.values():
+    #     print(member.ID, member.species)
+    #     if member.species == 'dove':
+    #         doves += 1
+    # print(doves)
     
 
 if __name__ == '__main__':
