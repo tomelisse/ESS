9e56cea2ac6f57ccf5f9afd5b1a8b664f01cd891
Maria Fabianska <tomelisse@gmail.com>
2017-08-22 18:42:35 +0200

    mass extinction not finished

--- doves_and_hawks.py
+++ doves_and_hawks.py
@@ -32,7 +32,7 @@ class bird:
 
     def duel(self, opponent):
         ''' duel between 2 individuals '''
-        results     = self.__class__._duels_rules[self.species, opponent.species]
+        results     = bird._duels_rules[self.species, opponent.species]
         self.HP     += results[0]
         opponent.HP += results[1]
         # print('duel: {} {}:{:d} vs {} {}:{:d}'.format(self.species, self.ID, self.HP, 
@@ -50,17 +50,22 @@ class bird:
             return 3
 
 class population:
-    _evolution_time  = 18
+    _evolution_time  = 15
+    _limit = 100000
+    # percentage of population perished during mass extinction
+    _extinction_factor = 0.5
     # number of duels wrt number of members
     _duels_percentage = 0.5
     ''' class for a population of birds '''
     def __init__(self):
         ''' population composition '''
-        n_doves = 4 
-        n_all   = 10
+        n_doves = 40 
+        n_all   = 100
         self.species = {'dove': 0, 'hawk' : 0, 'all' : 0}
-        # data for the plot
+        # data for the plots
         self.ratio = []
+        self.doves = []
+        self.hawks = []
         species = ['dove', 'hawk']
         self.members = dict()
         for i in range(n_all):
@@ -77,9 +82,10 @@ class population:
         self.species[species] += 1
         self.species['all'  ] += 1
 
-    def remove_member(self, id, species):
+    def remove_member(self, ID):
         ''' remove the given member from the population '''
-        del self.members[id]
+        species = self.members[ID].species
+        del self.members[ID]
         self.species[species] -= 1
         self.species['all']   -= 1
 
@@ -93,22 +99,40 @@ class population:
                 species = member.species
                 self.add_member(species)
 
-    # TODO: add population size limit
+    def mass_extinction(self):
+        ''' if population is overcrowded '''
+        all_members = self.species['all']
+        if all_members > population._limit:
+            print('->>> MASS EXTINCTION <<<-')
+            fatalities = int(population._extinction_factor*all_members)
+            IDs = self.members.keys()
+            for _ in range(fatalities):
+                ID = random.choice(IDs)
+                try:
+                    self.remove_member(ID)
+                except KeyError:
+                    IDs = self.members.keys()
+                    ID = random.choice(IDs)
+                    self.remove_member(ID)
+
+
+
     def extinction(self):
         ''' old and injured members die out, others get older '''
         print('-> extinction')
         died = 0
-        for member in list(self.members.values()):
+        members = list(self.members.values())
+        for member in list(members):
             member.age += 1
-            if member.HP < 0 or member.age >= member.__class__._max_age:
+            if member.HP < 0 or member.age >= bird._max_age:
                 # print('extict: {}, HP: {:d}, age: {:d}'.format(member.ID, member.HP, member.age))
-                self.remove_member(member.ID, member.species)
+                self.remove_member(member.ID)
                 died += 1
         print('died: ', died)
 
     def duels(self):
         ''' set up duels between pairs of members '''
-        n_duels = int(self.__class__._duels_percentage*self.species['all'])
+        n_duels = int(population._duels_percentage*self.species['all'])
         print('-> duels')
         # print('=== {} duels ==='.format(n_duels))
         repetitions = 0
@@ -125,18 +149,29 @@ class population:
         if float(repetitions)/float(n_duels) > 0.01:
             print('{} repetitions in {} duels'.format(repetitions, n_duels))
 
-    def update_plots(self):
+    def update_plot_data(self):
         ''' update info about the number of each species '''
-        self.ratio.append(float(self.species['dove'])/(self.species['hawk']))
+        doves = self.species['dove']
+        hawks = self.species['hawk']
+        self.doves.append(doves)
+        self.hawks.append(hawks)
+        self.ratio.append(float(doves)/float(hawks))
 
     def prepare_plots(self):
         ''' prepares plots '''
         plotpath = 'plots/ratio.png'
-        plt.plot(self.ratio, 'mo--', ms=5)
-        plt.xlabel('epochs')
-        plt.ylabel('ratios')
-        plt.text(2, 0.75, 'max_age: {}\nthresholds: {}, {}, {}'.format(bird._max_age, 
+        f, (ax1, ax2, ax3) = plt.subplots(3,1)
+        ax1.plot(self.ratio, 'mo--', ms=5)
+        # ax1.xlabel('epochs')
+        # ax1.ylabel('ratios')
+        ax1.text(2, 0.75, 'max_age: {}\nthresholds: {}, {}, {}'.format(bird._max_age, 
             bird._litter_thr1, bird._litter_thr2, bird._litter_thr3))
+        ax2.plot(self.doves, 'mo--', ms=5)
+        # ax2.xlabel('epochs')
+        # ax2.ylabel('doves')
+        ax3.plot(self.hawks, 'mo--', ms=5)
+        # ax3.xlabel('epochs')
+        # ax3.ylabel('hawks')
         plt.show()
         # plt.savefig(plotpath)
 
@@ -148,20 +183,22 @@ class population:
     def print_members(self):
         ''' print members '''
         # print('=== print members ===')
-        print('doves: ', self.species['dove'], 'hawks: ', self.species['hawk'])
+        species = self.species
+        print('all: ', species['all'], 'doves: ', species['dove'], 'hawks: ', species['hawk'])
         # for member in list(self.members.values()):
         #     print(member.ID, member.species, member.HP, member.age)
 
     def evolution(self):
         ''' simulates the evolution of the population '''
-        self.update_plots()
-        for epoch in range(self.__class__._evolution_time):
+        self.update_plot_data()
+        for epoch in range(population._evolution_time):
             print('======== epoch: {} ========'.format(epoch))
             self.print_members()
             self.duels()
             self.extinction()
             self.reproduction()
-            self.update_plots()
+            self.mass_extinction()
+            self.update_plot_data()
         self.print_members()
         self.prepare_plots()
 
@@ -170,12 +207,8 @@ def main():
     ''' main '''
     my_population = population()
     my_population.evolution()
-    # doves = 0
     # for member in my_population.members.values():
     #     print(member.ID, member.species)
-    #     if member.species == 'dove':
-    #         doves += 1
-    # print(doves)
     
 
 if __name__ == '__main__':
